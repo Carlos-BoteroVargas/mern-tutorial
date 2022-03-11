4c9a85fcf1afd67bed25e354d038c50a722666a0
Carlos Botero-Vargas <cbotero-vargas@outlook.com>
2022-03-11 14:17:19 -0500

    CRUD operations fully functional

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 3500;
+
+connectDB();
 
 const app = express();
 
@@ -12,4 +16,4 @@ app.use('/api/goals', require('./routes/goalRoutes'));
 
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`.yellow));
